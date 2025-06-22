c1f28856a8c1e79ac6dc0749f212a5f42dc35903
adamoski13 <adamjurczyk92@gmail.com>
2025-06-22 19:56:05 +0200

    Update rotate_op.c

--- move_op/rotate_op.c
+++ move_op/rotate_op.c
@@ -1,29 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_op.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajurczyk <ajurczyk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 20:06:27 by ajurczyk          #+#    #+#             */
+/*   Updated: 2025/06/14 20:13:08 by ajurczyk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../push_swap.h"
 
-void    ra(int *arr, int size)
+void	ra(int *arr, int size)
 {
-	int tmp;
+	int	tmp;
 
 	tmp = arr[0];
 	ft_memmove(arr, arr + 1, sizeof(int) * (size - 1));
 	arr[size - 1] = tmp;
-    ft_putstr_fd("ra\n", 1);
+	ft_putstr_fd("ra\n", 1);
 }
 
-void    rb(int *arr, int size)
+void	rb(int *arr, int size)
 {
-	int tmp;
+	int	tmp;
 
 	tmp = arr[0];
 	ft_memmove(arr, arr + 1, sizeof(int) * (size - 1));
 	arr[size - 1] = tmp;
-    ft_putstr_fd("rb\n", 1);
+	ft_putstr_fd("rb\n", 1);
 }
 
-void    rr(int *arr1, int *arr2, int size)
+void	rr(int *arr1, int *arr2, int size)
 {
-	int tmpa;
-	int tmpb;
+	int	tmpa;
+	int	tmpb;
 
 	tmpa = arr1[0];
 	ft_memmove(arr1, arr1 + 1, sizeof(int) * (size - 1));
@@ -31,5 +43,5 @@ void    rr(int *arr1, int *arr2, int size)
 	tmpb = arr2[0];
 	ft_memmove(arr2, arr2 + 1, sizeof(int) * (size - 1));
 	arr2[size - 1] = tmpb;
-    ft_putstr_fd("rr\n", 1);
+	ft_putstr_fd("rr\n", 1);
 }
