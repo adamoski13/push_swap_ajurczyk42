0741b3833fae370b8236e0981baeeebdbca56421
adamoski13 <adamjurczyk92@gmail.com>
2025-06-22 19:55:45 +0200

    Update rev_rotate_op.c

--- move_op/rev_rotate_op.c
+++ move_op/rev_rotate_op.c
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_rotate_op.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajurczyk <ajurczyk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 20:06:22 by ajurczyk          #+#    #+#             */
+/*   Updated: 2025/06/14 20:12:09 by ajurczyk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../push_swap.h"
 
-void    rra(int *arr, int size)
+void	rra(int *arr, int size)
 {
-	int tmp;
+	int	tmp;
 
 	tmp = arr[size - 1];
 	ft_memmove(arr + 1, arr, sizeof(int) * (size - 1));
 	arr[0] = tmp;
-    ft_putstr_fd("rra\n", 1);
+	ft_putstr_fd("rra\n", 1);
 }
 
-void    rrb(int *arr, int size)
+void	rrb(int *arr, int size)
 {
-	int tmp;
+	int	tmp;
 
 	tmp = arr[size - 1];
 	ft_memmove(arr + 1, arr, sizeof(int) * (size - 1));
 	arr[0] = tmp;
-    ft_putstr_fd("rrb\n", 1);
+	ft_putstr_fd("rrb\n", 1);
 }
