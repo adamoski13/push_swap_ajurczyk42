f330b4e9b1c0afc04b3377406804f1bc1da46f22
adamoski13 <adamjurczyk92@gmail.com>
2025-06-22 17:53:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 
 #include "push_swap.h"
 
-static void	join_args(int argc, char **argv, t_list *s)
+static void	join_args(int ac, char **av, t_list *s)
 {
 	char	*tmp;
 	char	*tmp2;
@@ -9,12 +9,12 @@ static void	join_args(int argc, char **argv, t_list *s)
 
 	i = 0;
 	tmp2 = ft_strdup("");
-	while (++i < argc && argv[i] != NULL)
+	while (++i < ac && av[i] != NULL)
 	{
-		tmp = ft_strjoin(tmp2, argv[i]);
+		tmp = ft_strjoin(tmp2, av[i]);
 		if (tmp2)
 			free(tmp2);
-		if (i != argc - 1)
+		if (i != ac - 1)
 		{
 			tmp2 = ft_strjoin(tmp, " ");
 			if (tmp)
@@ -24,25 +24,30 @@ static void	join_args(int argc, char **argv, t_list *s)
 	}
 	s->join_args = ft_strdup(tmp);
 	if (s->join_args == NULL)
-		free_and_exit_with_message(s, "Error\n");
+		free_stacks(s, "Error\n");
 	if (tmp)
 		free(tmp);
 }
 
-int	main(int argc, char **argv)
+void	fill_stack(t_list *s, int ac, char **av)
 {
-	t_list	*s;
+	initialize_list(ac, av, s);
+	join_args(ac, av, s);
+	parse_numbers(s);
+	exit_if_sorted_or_has_duplicate(s, 0);
+}
 
-	arg_check(argc, argv);
+int	main(int ac, char **av)
+{
+	t_list *s;
+
+	arg_check(ac, av);
 	s = malloc(sizeof * s);
 	if (s == NULL)
 		exit(1);
-	initialize_list(argc, argv, s);
-	join_args(argc, argv, s);
-	parse_numbers(s);
-	exit_if_sorted_or_has_duplicate(s, 0);
+	fill_stack(s, ac, av);
 	create_index(s);
-	if (s->a_size == 2 && s->a[0] > s->a[1])
+	if (s->a_size == 2)
 		sa(s->a);
 	else if (s->a_size == 3)
 		sort_three_elements(s);
@@ -51,6 +56,6 @@ int	main(int argc, char **argv)
 	else
 		over_six_sort(s);
 	exit_if_sorted_or_has_duplicate(s, 1);
-	free_and_exit_with_message(s, "Error\n");
+	free_stacks(s, "Error\n");
 	return (0);
 }
