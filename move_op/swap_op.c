a814e49f7ff2a845130e97f70bb1a5f5580536c9
adamoski13 <adamjurczyk92@gmail.com>
2025-06-22 19:56:21 +0200

    Update swap_op.c

--- move_op/swap_op.c
+++ move_op/swap_op.c
@@ -1,29 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap_op.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajurczyk <ajurczyk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 20:06:29 by ajurczyk          #+#    #+#             */
+/*   Updated: 2025/06/14 20:14:30 by ajurczyk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../push_swap.h"
 
-void    sa(int *arr)
+void	sa(int *arr)
 {
 	int	tmp;
 
 	tmp = arr[0];
 	arr[0] = arr[1];
 	arr[1] = tmp;
-   	ft_putstr_fd("sa\n", 1);
+	ft_putstr_fd("sa\n", 1);
 }
 
-void    sb(int *arr)
+void	sb(int *arr)
 {
 	int	tmp;
 
 	tmp = arr[0];
 	arr[0] = arr[1];
 	arr[1] = tmp;
-   	ft_putstr_fd("sb\n", 1);
+	ft_putstr_fd("sb\n", 1);
 }
 
-void    ss(int *arr1, int *arr2)
+void	ss(int *arr1, int *arr2)
 {
 	int	tmpa;
-	int tmpb;
+	int	tmpb;
 
 	tmpa = arr1[0];
 	arr1[0] = arr1[1];
@@ -31,5 +43,5 @@ void    ss(int *arr1, int *arr2)
 	tmpb = arr2[0];
 	arr2[0] = arr2[1];
 	arr2[1] = tmpb;
-    ft_putstr_fd("ss\n", 1);
+	ft_putstr_fd("ss\n", 1);
 }
