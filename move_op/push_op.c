8fd0c231ad85d8304cc711c6f058126e6e4013d6
adamoski13 <adamjurczyk92@gmail.com>
2025-06-22 19:55:28 +0200

    Update push_op.c

--- move_op/push_op.c
+++ move_op/push_op.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_op.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajurczyk <ajurczyk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 20:06:16 by ajurczyk          #+#    #+#             */
+/*   Updated: 2025/06/14 20:14:22 by ajurczyk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../push_swap.h"
 
-void    pa(t_list *s)
+void	pa(t_list *s)
 {
-    int tmp;
+	int	tmp;
 
 	tmp = s->b[0];
 	ft_memmove(s->a + 1, s->a, sizeof(int) * s->a_size);
@@ -10,12 +22,12 @@ void    pa(t_list *s)
 	s->b_size--;
 	ft_memmove(s->b, s->b + 1, sizeof(int) * s->b_size);
 	s->a_size++;
-    ft_putstr_fd("pa\n", 1);
+	ft_putstr_fd("pa\n", 1);
 }
 
-void    pb(t_list *s)
+void	pb(t_list *s)
 {
-    int tmp;
+	int	tmp;
 
 	tmp = s->a[0];
 	ft_memmove(s->b + 1, s->b, sizeof(int) * s->b_size);
@@ -23,5 +35,5 @@ void    pb(t_list *s)
 	s->a_size--;
 	ft_memmove(s->a, s->a + 1, sizeof(int) * s->a_size);
 	s->b_size++;
-    ft_putstr_fd("pb\n", 1);
+	ft_putstr_fd("pb\n", 1);
 }
